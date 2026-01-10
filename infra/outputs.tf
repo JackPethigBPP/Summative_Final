a7e7048099a54a25d1236ec6eb84b4a9cf9fcbec
Amy Grace Willmott <amy@amys-air.home>
2026-01-10 14:44:45 +0000

    spelling mistake

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
 
 output "alb_dns" { value = module.alb.dns_name }
-output "ecr_repo_url" { value = module.ecr.repository_url }
+output "ecr_repository_url" { value = module.ecr.repository_url }
 output "rds_endpoint" { value = try(module.rds.endpoint, null) }
 output "target_group_arn" { value = module.alb.target_group_arn }
