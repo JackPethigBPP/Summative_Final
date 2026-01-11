45043c2dfba5412e5dd36763a7645d0a3a3c1284
Amy Grace Willmott <amy@unknown.home>
2026-01-11 14:37:57 +0000

    Autoscaling upon each workflow

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -4,3 +4,4 @@ output "ecr_repository_url" { value = module.ecr.repository_url }
 output "rds_endpoint" { value = try(module.rds.endpoint, null) }
 output "target_group_arn" { value = module.alb.target_group_arn }
 output "vpc_id" { value = module.vpc.vpc_id }
+output "asg_name" { value = aws_autoscaling_group.app.name }
